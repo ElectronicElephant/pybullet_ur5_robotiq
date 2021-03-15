ee899926a0027a797c9a874bf2391592158aedfc
tutian <1489272013@qq.com>
2021-03-15 18:45:27 +0800

    Clean code

--- main.py
+++ main.py
@@ -6,34 +6,7 @@ import pybullet as p
 from tqdm import tqdm
 from env import ClutteredPushGrasp
 from utilities import YCBModels, Camera
-
-
-def heuristic_demo():
-    ycb_models = YCBModels(
-        os.path.join('./data/ycb', '**', 'textured-decmp.obj'),
-    )
-    camera = Camera((0, -0.5, 1.5), 0.1, 5, (320, 320), 40)
-
-    env = ClutteredPushGrasp(ycb_models, camera, vis=True, num_objs=5, gripper_type='85')
-    p.resetDebugVisualizerCamera(2.0, -270., -60., (0., 0., 0.))
-    p.configureDebugVisualizer(p.COV_ENABLE_SHADOWS, 1)  # Shadows on/off
-
-    (rgb, depth, seg) = env.reset()
-    step_cnt = 0
-    while True:
-
-        h_, w_ = np.unravel_index(depth.argmin(), depth.shape)
-        x, y, z = camera.rgbd_2_world(w_, h_, depth[h_, w_])
-
-        p.addUserDebugLine([x, y, 0], [x, y, z], [0, 1, 0])
-        p.addUserDebugLine([x, y, z], [x, y, z+0.05], [1, 0, 0])
-
-        (rgb, depth, seg), reward, done, info = env.step((x, y, z), 1, 'grasp')
-
-        print('Step %d, grasp at %.2f,%.2f,%.2f, reward %f, done %s, info %s' %
-              (step_cnt, x, y, z, reward, done, info))
-        step_cnt += 1
-        # time.sleep(3)
+import time
 
 
 def user_control_demo():
@@ -41,27 +14,21 @@ def user_control_demo():
         os.path.join('./data/ycb', '**', 'textured-decmp.obj'),
     )
     camera = Camera((0, -0.5, 1.5), 0.1, 5, (320, 320), 40)
+    camera = None
 
-    env = ClutteredPushGrasp(ycb_models, camera, vis=True, num_objs=0, gripper_type='85')
+    env = ClutteredPushGrasp(ycb_models, camera, vis=True)
     p.resetDebugVisualizerCamera(2.0, -270., -60., (0., 0., 0.))
     p.configureDebugVisualizer(p.COV_ENABLE_SHADOWS, 1)  # Shadows on/off
     p.addUserDebugLine([0, -0.5, 0], [0, -0.5, 1.1], [0, 1, 0])
 
     env.reset()
+    env.SIMULATION_STEP_DELAY = 1 / 240.
     while True:
-        env.step(None, None, None, True)
-
-        # key control
-        keys = p.getKeyboardEvents()
-        # key "Z" is down and hold
-        if (122 in keys) and (keys[122] == 3):
-            print('Grasping...')
-            if env.close_gripper(check_contact=True):
-                print('Grasped!')
-        # key R
-        if 114 in keys:
-            env.open_gripper()
-        # time.sleep(1 / 120.)
+        for _ in range(240):
+            # env.step(env.read_debug_parameter(), 'end')
+            env.step((0, 0, 0.2, 0, 3.14, -3.14 / 2, 0.02), 'end')
+        for _ in range(240):
+            env.step([0.98, 0.458, 0.31, -2.24, -0.30, 2.66, 2.32, 0.02], 'joint')
 
 
 if __name__ == '__main__':
