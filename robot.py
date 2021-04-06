b3a3803c469f4fe850e664808fcc8613abc6c35d
tutian <1489272013@qq.com>
2021-04-06 16:26:24 +0800

    Add Robotiq140

--- robot.py
+++ robot.py
@@ -197,3 +197,16 @@ class UR5Robotiq85(RobotBase):
         for joint_id, multiplier in self.mimic_child_multiplier.items():
             p.setJointMotorControl2(self.id, joint_id, p.POSITION_CONTROL, targetPosition=open_angle * multiplier,
                                     force=self.joints[joint_id].maxForce, maxVelocity=self.joints[joint_id].maxVelocity)
+
+
+class UR5Robotiq140(UR5Robotiq85):
+    def __init_robot__(self):
+        self.eef_id = 7
+        self.arm_num_dofs = 6
+        self.arm_rest_poses = [-1.5690622952052096, -1.5446774605904932, 1.343946009733127, -1.3708613585093699,
+                               -1.5707970583733368, 0.0009377758247187636]
+        self.id = p.loadURDF("./urdf/ur5_robotiq_140.urdf", self.base_pos, self.base_ori,
+                             useFixedBase=True, flags=p.URDF_ENABLE_CACHED_GRAPHICS_SHAPES)
+        self.gripper_range = [0, 0.085]
+        # TODO: It's weird to use the same range and the same formula to calculate open_angle as Robotiq85.
+
